b7e57c7717570386f1df99e71aef5eb3508a2a8c
Gallay Lajos <gallay.lajos@live.com>
2017-05-14 16:27:13 +0200

    ServerActionOwnerAbstract tests, other test class skeletons

---
+++ test/serveractionownerabstract.test.ts
@@ -0,0 +1,55 @@
+import * as chai from 'chai';
+import { CustomAction, ForeignKey, PrimaryKey } from 'furystack-core';
+import { suite, test } from 'mocha-typescript';
+import { ServerActionOwnerAbstract, ServerCustomAction } from '../src/ServerScoped/index';
+import { TestHelpers } from './';
+
+class TestClass {
+    constructor() {
+        //
+    }
+}
+
+class ImplementedActionOwner extends ServerActionOwnerAbstract {
+    protected getActionByName<TBody, TReturns>(name: string): CustomAction<TBody, TReturns> {
+        return new CustomAction(name, 'GET', TestClass, TestClass) as CustomAction<TBody, TReturns>;
+    }
+
+}
+
+@suite()
+export class ServerActionOwnerAbstractTest {
+
+    @test('Should be constructed')
+    public Construct() {
+        const actionOwner = new ImplementedActionOwner();
+    }
+
+    @test('Add and Get Method with implementation')
+    public AddAndGetImplementation() {
+        const actionOwner = new ImplementedActionOwner();
+        actionOwner.ImplementAction<TestClass, TestClass>('testCustomAction', async  (arg, req) => {
+            return new TestClass();
+        });
+
+        const found = actionOwner.GetActions().find((a) => a.Name === 'testCustomAction');
+        chai.expect(found.Name).to.be.eq('testCustomAction');
+        chai.expect(found.BodyType).to.be.eq(TestClass);
+        chai.expect(found.BodyTypeName).to.be.eq('TestClass');
+        chai.expect(found.ReturnsType).to.be.eq(TestClass);
+        chai.expect(found.ReturnTypeName).to.be.eq('TestClass');
+        chai.expect(found.RequestType).to.be.eq('GET');
+    }
+
+    @test('AddAndGetMethodImplementation')
+    public ImplementTwiceThrowError() {
+        const actionOwner = new ImplementedActionOwner();
+        const implementFunc = () => {
+            actionOwner.ImplementAction<TestClass, TestClass>('testCustomAction', async  (arg, req) => {
+                return new TestClass();
+            });
+        };
+        implementFunc();
+        chai.expect(implementFunc).to.be.throw();
+    }
+}
---
+++ test/serverentitytype.test.ts
@@ -0,0 +1,13 @@
+import * as chai from 'chai';
+import { ForeignKey, PrimaryKey } from 'furystack-core';
+import { suite, test } from 'mocha-typescript';
+import { TestHelpers } from './';
+
+@suite()
+export class ServerEntityTypeTest {
+
+    @test('Should be constructed')
+    public Construct() {
+
+    }
+}
