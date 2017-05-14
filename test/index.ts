6d47f413263307a45f9bfc959c3de1171587408d
Gallay Lajos <gallay.lajos@live.com>
2017-05-14 16:58:26 +0200

    remove: servercustomaction test

--- test/index.ts
+++ test/index.ts
@@ -3,6 +3,5 @@ export * from './testhelpers';
 export * from './endpointroute.test';
 export * from './inmemorystore.test';
 export * from './serveractionownerabstract.test';
-export * from './servercustomaction.test';
 export * from './serverentityset.test';
 export * from './serverentitytype.test';
