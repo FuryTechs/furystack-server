b232f6ba2fec968e91c553cd0a712f6e2d664857
Gallay Lajos <gallay.lajos@live.com>
2017-05-14 16:50:03 +0200

    Add: ServerEntityType test

--- test/serverentitytype.test.ts
+++ test/serverentitytype.test.ts
@@ -1,13 +1,29 @@
 import * as chai from 'chai';
-import { ForeignKey, PrimaryKey } from 'furystack-core';
+import { EndpointEntityType, ForeignKey, ModelDescriptorStore, PrimaryKey, Property } from 'furystack-core';
 import { suite, test } from 'mocha-typescript';
+import { ServerEntityType } from '../src/ServerScoped/index';
 import { TestHelpers } from './';
 
+class EntityTypeTestClass {
+    @Property
+    public Value: string;
+}
 @suite()
 export class ServerEntityTypeTest {
 
     @test('Should be constructed')
     public Construct() {
+        const et = new EndpointEntityType('TestClass', ModelDescriptorStore.GetDescriptor(EntityTypeTestClass));
+        const entityType = new ServerEntityType(et);
+    }
 
+    @test('Should be constructed')
+    public ImplementAction() {
+        const et = new EndpointEntityType('TestClass', ModelDescriptorStore.GetDescriptor(EntityTypeTestClass));
+        et.CustomAction('customAction', 'GET', Number, Number);
+        const entityType = new ServerEntityType(et);
+        const action = entityType.ImplementAction('customAction', async (arg, req) => {
+            return 1;
+        });
     }
 }
